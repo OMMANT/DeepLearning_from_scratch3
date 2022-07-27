bd11f0db3e280fcf8d6ee9a5c833262bdf25bb39
OMMANT <ommant@naver.com>
2022-07-27 18:52:49 +0900

    6단계 내용 추가

--- src/function.py
+++ src/function.py
@@ -2,20 +2,35 @@ import numpy as np
 from variable import Variable
 
 class Function:
-    def __call__(self, input: Variable):
+    def __call__(self, input: Variable) -> Variable:
+        self.input = input
         x = input.data
         y = self.forward(x)
         output = Variable(y)
 
         return output
     
-    def forward(self, x):
+    def forward(self, x: np.ndarray):
+        raise NotImplementedError()
+    
+    def backward(self, gy: np.ndarray):
         raise NotImplementedError()
 
 class Square(Function):
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> np.ndarray:
         return np.square(x)
 
+    def backward(self, gy: np.ndarray) -> np.ndarray:
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
 class Exp(Function):
-    def forward(self, x):
+    def forward(self, x: np.ndarray) -> np.ndarray:
         return np.exp(x)
+    
+    def backward(self, gy: np.ndarray) -> np.ndarray:
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+        
--- src/variable.py
+++ src/variable.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 class Variable:
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray):
         self.data = data
+        self.grad = None
