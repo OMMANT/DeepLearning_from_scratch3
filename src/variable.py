684899247cbf5e3ced523b89f623cad389066df6
OMMANT <ommant@naver.com>
2022-08-18 15:40:08 +0900

    8단계 내용 추가

--- src/variable.py
+++ src/variable.py
@@ -9,3 +9,14 @@ class Variable:
 
     def set_creator(self, func: Function):
         self.creator = func
+
+    def backward(self):
+        funcs = [self.creator]
+        
+        while funcs:
+            f = funcs.pop() # 함수를 가져온다
+            x, y = f.input, f.output    # 함수의 입력과 출력을 가져온다.
+            x.grad = f.backward(y.grad) # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                funcs.append(x.creator)
